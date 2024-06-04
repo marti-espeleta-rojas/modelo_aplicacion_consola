739c03d39d86dbe0978ec0a5857647ec9b0e06d2
marti-espeleta-rojas <162464801+marti-espeleta-rojas@users.noreply.github.com>
2024-06-04 20:25:01 -0300

    Windows Form solución de promedio de notas

---
+++ Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WindowsFormsAppClase4_6_24
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbNota = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbPromedio = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbNota);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(26, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(296, 114);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Solicitud de Ingreso de Nueva Nota";
+            // 
+            // tbNota
+            // 
+            this.tbNota.Location = new System.Drawing.Point(184, 36);
+            this.tbNota.Name = "tbNota";
+            this.tbNota.Size = new System.Drawing.Size(100, 20);
+            this.tbNota.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(100, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Agregar Nota";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnIngresarNota_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese a continuación la nota";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lbPromedio);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(26, 156);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(296, 125);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Consulta del Promedio General";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(84, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Consulta Promedio";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.ConsultaPromedio_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "El promedio resultante es:";
+            // 
+            // lbPromedio
+            // 
+            this.lbPromedio.AutoSize = true;
+            this.lbPromedio.Location = new System.Drawing.Point(170, 28);
+            this.lbPromedio.Name = "lbPromedio";
+            this.lbPromedio.Size = new System.Drawing.Size(0, 13);
+            this.lbPromedio.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 311);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Modelo de Aplicación de Ventanas";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbNota;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lbPromedio;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppClase4_6_24
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region variables estáticas y métodos del dominio del problema
+        static int cont;
+        static double acum;
+        static double CalcularPromedio()
+        {
+            double promedio = 0;
+            if (cont > 0)
+            {
+                promedio = acum / cont;
+            }
+            return promedio;
+        }
+        static void RegistrarNota(double nota)
+        {
+            acum+= nota;
+            cont++;
+        }
+        #endregion
+
+        private void btnIngresarNota_Click(object sender, EventArgs e)
+        {
+            double nota = Convert.ToDouble(tbNota.Text);
+            RegistrarNota(nota);
+            tbNota.Clear();
+        }
+        private void ConsultaPromedio_Click(object sender, EventArgs e)
+        {
+            double promedio=CalcularPromedio();
+            lbPromedio.Text = promedio.ToString("0.00");
+        }
+    
+    }
+
+}
